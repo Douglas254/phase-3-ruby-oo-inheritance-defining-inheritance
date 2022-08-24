96ba96e545bbd7b85ae1bb85436833651c7129a7
Douglas Obara <douglasobara97@gmail.com>
2022-08-24 10:20:09 +0300

    commit tree 1a1fe9802e5358dd5f701667fe11dc7eeb5c7ae5

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,20 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+# Car
+# .new
+# initializes with two arguments, wheel size and number
+# #wheel_size
+# has a wheel_size
+# #wheel_number
+# has a wheel_number
+# #go
+# returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+# #fill_up_tank
+# returns 'filling up!'
+# inheriting everything from vehicle
+class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
